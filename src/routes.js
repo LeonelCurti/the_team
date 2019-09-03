0906f02eefebc01f280647c8ed82bb46d898f183
Leonel Curti <leonelcurti@gmail.com>
2019-09-02 22:14:01 -0300

    clear structure, add header and footer

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./Hoc/Layout";
+
+const Routes = props => {
+  return (
+    <Layout>
+      {props.children}
+    </Layout>
+  )
+};
+
+export default Routes;
