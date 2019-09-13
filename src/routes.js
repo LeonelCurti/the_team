4245a229736f892d619290ec801bac148ac26273
Leonel Curti <leonelcurti@gmail.com>
2019-09-12 22:24:15 -0300

    starting auth in dasboard

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import {Switch, Route} from "react-router-dom";
 import Layout from "./Hoc/Layout";
 
 import Home from './Components/home';
+import SignIn from './Components/signin';
+
+import Dashboard from './Components/admin/Dashboard';
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path='/dashboard'/>
+        <Route exact component={SignIn} path='/signin'/>
         <Route exact component={Home} path='/'/>
       </Switch>
     </Layout>
