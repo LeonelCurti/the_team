490fbbc65546e9a0857df6bfc2428c0ad3fe6c6a
Leonel Curti <leonelcurti@gmail.com>
2019-09-25 00:26:18 -0300

    the team route and firebase loading images

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,27 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import TheTeam from './Components/theTeam';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
 import AddEditMatch from './Components/admin/matches/addEditMatch';
+import AdminPlayers from './Components/admin/players';
+import AddEditPlayers from './Components/admin/players/addEditPlayers';
 
 const Routes = props => {
 
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path='/admin_players/add_players' exact component={AddEditPlayers}/> 
+        <PrivateRoute {...props} path='/admin_players/add_players/:id' exact component={AddEditPlayers}/> 
+        <PrivateRoute {...props} path='/admin_players' exact component={AdminPlayers}/> 
         <PrivateRoute {...props} path='/admin_matches/edit_match' exact component={AddEditMatch}/> 
         <PrivateRoute {...props} path='/admin_matches/edit_match/:id' exact component={AddEditMatch}/> 
         <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/> 
         <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>    
+        <PublicRoute {...props} restricted={false} path='/the_team' exact component={TheTeam}/> 
         <PublicRoute {...props} restricted={true} path='/signin' exact component={SignIn}/> 
         <PublicRoute {...props} restricted={false} path='/' exact component={Home}/> 
       </Switch>
