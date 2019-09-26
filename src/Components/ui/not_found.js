45d02c20fa9271e94a43e8b6021f4caa65e9cba1
Leonel Curti <leonelcurti@gmail.com>
2019-09-25 23:40:19 -0300

    finishing matches filters and 404 page

---
+++ src/Components/ui/not_found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='not_found_container'>
+      <div>Page Not Found</div>
+    </div>
+  )
+}
+
+export default NotFound
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import TheTeam from './Components/theTeam';
+import TheMatches from './Components/theMatches';
+import NotFound from './Components/ui/not_found';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
@@ -27,9 +29,11 @@ const Routes = props => {
         <PrivateRoute {...props} path='/admin_matches/edit_match/:id' exact component={AddEditMatch}/> 
         <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/> 
         <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>    
+        <PublicRoute {...props} restricted={false} path='/the_matches' exact component={TheMatches}/> 
         <PublicRoute {...props} restricted={false} path='/the_team' exact component={TheTeam}/> 
         <PublicRoute {...props} restricted={true} path='/signin' exact component={SignIn}/> 
         <PublicRoute {...props} restricted={false} path='/' exact component={Home}/> 
+        <PublicRoute {...props} restricted={false} component={NotFound}/> 
       </Switch>
     </Layout>
   )
